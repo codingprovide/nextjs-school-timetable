62bce613c012c3d057b30767bb1a700d27816ca7
weijun su <codingprovide@gmail.com>
2024-02-27 00:31:22 +0800

    feat:增加課程內容滑動功能

---
+++ src/app/data/courseDataList.ts
@@ -0,0 +1,59 @@
+export const initialCourseData = {
+  Sun: [
+    { course: "國文", classroom: "國秀樓", classPeriodNumber: 10 },
+    { course: "國文", classroom: "國秀樓", classPeriodNumber: 11 },
+    { course: "國文", classroom: "國秀樓", classPeriodNumber: 12 },
+    { course: "國文", classroom: "國秀樓", classPeriodNumber: 13 },
+    { course: "國文", classroom: "國秀樓", classPeriodNumber: 14 },
+  ],
+  Mon: [
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 10 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 11 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 12 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 13 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 14 },
+  ],
+  Tue: [
+    { course: "英文", classroom: "國秀樓", classPeriodNumber: 10 },
+    { course: "英文", classroom: "國秀樓", classPeriodNumber: 11 },
+    { course: "英文", classroom: "國秀樓", classPeriodNumber: 12 },
+    { course: "英文", classroom: "國秀樓", classPeriodNumber: 13 },
+    { course: "英文", classroom: "國秀樓", classPeriodNumber: 14 },
+  ],
+  Wed: [
+    { course: "數位邏輯", classroom: "國秀樓", classPeriodNumber: 10 },
+    { course: "數位邏輯", classroom: "國秀樓", classPeriodNumber: 11 },
+    { course: "數位邏輯", classroom: "國秀樓", classPeriodNumber: 12 },
+    { course: "數位邏輯", classroom: "國秀樓", classPeriodNumber: 13 },
+    { course: "數位邏輯", classroom: "國秀樓", classPeriodNumber: 14 },
+  ],
+  Thu: [
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 10 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 11 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 12 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 13 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 14 },
+  ],
+  Fri: [
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 10 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 11 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 12 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 13 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 14 },
+  ],
+  Sat: [
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 10 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 11 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 12 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 13 },
+    { course: "數學", classroom: "國秀樓", classPeriodNumber: 14 },
+  ],
+};
+
+export const classScheduleList = [
+  { classPeriod: "第10節", classTime: "18:10-18:55", classPeriodNumber: 10 },
+  { classPeriod: "第11節", classTime: "19:10-18:55", classPeriodNumber: 11 },
+  { classPeriod: "第12節", classTime: "19:50-20:35", classPeriodNumber: 12 },
+  { classPeriod: "第13節", classTime: "20:40-21:25", classPeriodNumber: 13 },
+  { classPeriod: "第14節", classTime: "21:30-22:15", classPeriodNumber: 14 },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,45 +2,102 @@
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import DateWeekLayout from "./components/DateWeekLayout";
 import TodayDisplay from "./components/TodayDisplay";
-import { format, eachDayOfInterval, startOfWeek, endOfWeek } from "date-fns";
+import {
+  format,
+  eachDayOfInterval,
+  startOfWeek,
+  endOfWeek,
+  subDays,
+  addDays,
+} from "date-fns";
 import { zhTW } from "date-fns/locale/zh-TW";
-import { useEffect, useState } from "react";
+import { FormEvent, use, useEffect, useState } from "react";
 import WeekList from "./components/WeekList";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-
+import SwipeableViews from "react-swipeable-views";
+import { initialCourseData, classScheduleList } from "./data/courseDataList";
 export default function Home() {
   const [currentDate, setCurrentDate] = useState(new Date());
+  interface CourseData {
+    [key: string]: {
+      course: string;
+      classroom: string;
+      classPeriodNumber: number;
+    }[];
+  }
+  const [courseDate, setCourseDate] = useState<CourseData>(initialCourseData);
+  const [courseRenderIndex, setCourseRenderIndex] = useState(1);
+  const handleChangeIndex = (index: any) => {
+    console.log("Changed to index:", index);
+    console.log("courseRenderIndex", courseRenderIndex);
+  };
+
+  const [yesterday, setYesterday] = useState(subDays(currentDate, 1));
+  const [yesterdayOfweekShort, setYesterdayOfweekShort] = useState(
+    format(yesterday, "E")
+  );
+  const [tomorrow, setTomorrow] = useState(addDays(currentDate, 1));
+  const [tomorrowOfWeekShort, setTomorrowOfWeekShort] = useState(
+    format(tomorrow, "E")
+  );
+  const [currentDayOfWeekShort, setCurrentDayOfWeekShort] = useState(
+    format(currentDate, "E")
+  );
+  const [courseRender, setCourseRender] = useState([
+    courseDate[yesterdayOfweekShort],
+    courseDate[currentDayOfWeekShort],
+    courseDate[tomorrowOfWeekShort],
+  ]);
+  const [currentToday, setCurretToday] = useState(format(currentDate, "d"));
+  const [currentWeek, setCurrentWeek] = useState(
+    format(currentDate, "eeee", { locale: zhTW })
+  );
+  const [currentMonth, setCurrentMonth] = useState(
+    format(currentDate, "MMMM", { locale: zhTW })
+  );
+  const [currentYear, setCurrentYear] = useState(format(currentDate, "yyyy"));
+  const [dayOfweeks, setDayOfWeeks] = useState(
+    eachDayOfInterval({
+      start: startOfWeek(currentDate),
+      end: endOfWeek(currentDate),
+    })
+  );
+  useEffect(() => {
+    setCurretToday(format(currentDate, "d"));
+    setCurrentMonth(format(currentDate, "MMMM", { locale: zhTW }));
+    setCurrentYear(format(currentDate, "yyyy"));
+    setCurrentWeek(format(currentDate, "eeee", { locale: zhTW }));
+    const yesterdayDate = subDays(currentDate, 1);
+    const tomorrowDate = addDays(currentDate, 1);
+    const yesterdayOfweekShort = format(yesterdayDate, "E");
+    const tomorrowOfWeekShort = format(tomorrowDate, "E");
+    const currentDayOfWeekShort = format(currentDate, "E");
+    setYesterday(yesterdayDate);
+    setYesterdayOfweekShort(yesterdayOfweekShort);
+    setTomorrow(tomorrowDate);
+    setTomorrowOfWeekShort(tomorrowOfWeekShort);
+    setCurrentDayOfWeekShort(currentDayOfWeekShort);
+    setCourseRenderIndex(1);
+    setDayOfWeeks(
+      eachDayOfInterval({
+        start: startOfWeek(currentDate),
+        end: endOfWeek(currentDate),
+      })
+    );
+    setCourseRender([
+      courseDate[yesterdayOfweekShort],
+      courseDate[currentDayOfWeekShort],
+      courseDate[tomorrowOfWeekShort],
+    ]);
+  }, [courseDate, currentDate]);
+
   useEffect(() => {
     setCurrentDate(new Date());
   }, []);
 
-  const currentToday = format(currentDate, "d");
-  const currentWeek = format(currentDate, "eeee", { locale: zhTW });
-  const currentMonth = format(currentDate, "MMMM", { locale: zhTW });
-  const currentYear = format(currentDate, "yyyy");
-  const dayOfweeks = eachDayOfInterval({
-    start: startOfWeek(currentDate),
-    end: endOfWeek(currentDate),
-  });
   const handleChangeDate = (date: Date) => {
     setCurrentDate(date);
   };
-
-  const classScheduleList = [
-    { classPeriod: "第10節", classTime: "18:10-18:55", classPeriodNumber: 10 },
-    { classPeriod: "第11節", classTime: "19:10-18:55", classPeriodNumber: 11 },
-    { classPeriod: "第12節", classTime: "19:50-20:35", classPeriodNumber: 12 },
-    { classPeriod: "第13節", classTime: "20:40-21:25", classPeriodNumber: 13 },
-    { classPeriod: "第14節", classTime: "21:30-22:15", classPeriodNumber: 14 },
-  ];
-
-  const courseList = [
-    { course: "國文", classroom: "國秀樓", classPeriodNumber: 10 },
-    { course: "國文", classroom: "國秀樓", classPeriodNumber: 11 },
-    { course: "國文", classroom: "國秀樓", classPeriodNumber: 12 },
-    { course: "國文", classroom: "國秀樓", classPeriodNumber: 13 },
-    { course: "國文", classroom: "國秀樓", classPeriodNumber: 14 },
-  ];
 
   return (
     <Container maxWidth={"lg"} className=" p-0">
@@ -67,7 +124,12 @@ export default function Home() {
               className=" w-1/4 border-0 border-r-2 border-solid border-slate-200 m-0"
             >
               {classScheduleList.map((data) => (
-                <Grid2 xs={12} key={data.classPeriodNumber}>
+                <Grid2
+                  xs={12}
+                  key={data.classPeriodNumber}
+                  p={0}
+                  className=" flex justify-center items-center flex-col"
+                >
                   <Typography
                     variant="body1"
                     fontSize={12}
@@ -82,26 +144,43 @@ export default function Home() {
                 </Grid2>
               ))}
             </Grid2>
-            <Grid2 container spacing={4} gap={2} className="w-3/4 m-0 px-2">
-              {courseList.map((data, index) => (
+            <SwipeableViews
+              className=" w-3/4"
+              enableMouseEvents
+              index={courseRenderIndex}
+              onChangeIndex={handleChangeIndex}
+            >
+              {courseRender.map((course, courseIndex) => (
                 <Grid2
-                  key={index}
-                  xs={12}
-                  className="  border-red-300 border-l-8 rounded-lg border-solid border-0 bg-red-50"
+                  container
+                  spacing={4}
+                  gap={2}
+                  className="w-full m-0 px-2"
+                  key={courseIndex}
                 >
-                  <Stack direction={"column"}>
-                    <Stack
-                      direction={"row"}
-                      justifyContent={"center"}
-                      alignItems={"center"}
+                  {course.map((data, index) => (
+                    <Grid2
+                      key={index}
+                      xs={12}
+                      className="border-red-300 border-l-8 rounded-lg border-solid border-0 bg-red-50"
                     >
-                      {data.course}
-                    </Stack>
-                    {data.classroom}
-                  </Stack>
+                      <Stack direction="column">
+                        <Stack
+                          direction="row"
+                          justifyContent="center"
+                          alignItems="center"
+                        >
+                          {data.course}
+                        </Stack>
+                        <Typography variant="body1">
+                          {data.classroom}
+                        </Typography>
+                      </Stack>
+                    </Grid2>
+                  ))}
                 </Grid2>
               ))}
-            </Grid2>
+            </SwipeableViews>
           </Stack>
         </Box>
       </Box>
