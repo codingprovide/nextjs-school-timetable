61873aa30edc30448c0b4f08098a69986ce46c42
weijun su <codingprovide@gmail.com>
2024-03-05 01:06:52 +0800

    fix:修正課表時間

--- src/app/data/courseDataList.ts
+++ src/app/data/courseDataList.ts
@@ -135,7 +135,7 @@ export const initialCourseData = {
 
 export const classScheduleList = [
   { classPeriod: "第10節", classTime: "18:10-18:55", classPeriodNumber: 10 },
-  { classPeriod: "第11節", classTime: "19:10-18:55", classPeriodNumber: 11 },
+  { classPeriod: "第11節", classTime: "19:00-19:45", classPeriodNumber: 11 },
   { classPeriod: "第12節", classTime: "19:50-20:35", classPeriodNumber: 12 },
   { classPeriod: "第13節", classTime: "20:40-21:25", classPeriodNumber: 13 },
   { classPeriod: "第14節", classTime: "21:30-22:15", classPeriodNumber: 14 },
